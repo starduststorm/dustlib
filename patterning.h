8ffaa80ffce3062e7d607c4cacb52d23d92ae539
Opal Holley <lumin@escent.email>
2026-09-21 19:00:42 -0700

    move SoundPattern into dustlib for consistency/gain management

--- patterning.h
+++ patterning.h
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <drawing.h>
 #include <paletting.h>
+#include <audio.h>
 
 using ColorManager = PaletteRotation<CRGBPalette256>;
 #if DUSTLIB_SHARED_COLORMANAGER
@@ -144,6 +145,59 @@ public:
   void update() { }
   const char *description() {
     return "BlankPattern";
+  }
+};
+
+class SoundPattern : public Pattern, public FFTReceiver {
+public:
+  unsigned long lastLevelThreshChange{0};
+  int minFFTLevelThreshold{3};
+  int fftLevelThreshold{minFFTLevelThreshold};
+  int autoGainAdjustmentInterval{600};
+
+  SoundPattern() : FFTReceiver(fftProcessing) { }
+
+  void autoGainUpdate() {
+    FFTFrame frame = fftProcessing.getDataFrame();
+    unsigned long mils = millis();
+
+    int maxFrameValue = 0;
+    int32_t sumFrameValue = 0;
+    for (int i = 0 ; i < frame.size; ++i) {
+      if (frame.smoothSpectrum[i] > maxFrameValue) {
+        maxFrameValue = frame.smoothSpectrum[i];
+      }
+      sumFrameValue += frame.smoothSpectrum[i];
+    }
+    int avgFrameValue = sumFrameValue/frame.size;
+
+    int litCount{0};
+    for (int i = 0 ; i < LED_COUNT; ++i) {
+      litCount += ctx.leds[i] ? 1 : 0;
+    }
+    /* latch-ditch auto gain:
+     * slowly adjust threshold for drawing if to approach the average levels
+     * quickly move threshold for drawing if we're over- or under-drawing
+     * temporarily adjust thresholds at a fast interval at the start of pattern running to find a baseline
+    */
+   bool overDrawing = litCount > 95*LED_COUNT/100;
+   bool underDrawing = litCount < 2*LED_COUNT/10;
+   int adjustmentInterval = (runTime() > 3000 ? autoGainAdjustmentInterval : autoGainAdjustmentInterval/6);
+   if ((overDrawing || fftLevelThreshold < avgFrameValue) && mils - lastLevelThreshChange > adjustmentInterval) {
+      fftLevelThreshold++;
+      if (overDrawing) {
+        fftLevelThreshold += max(0, (maxFrameValue - fftLevelThreshold) / 20);
+      }
+      // logf("SoundPattern litCount = %i, frame value avg=%i,max=%i, fftLevelThreshold up to %i", litCount, avgFrameValue, maxFrameValue, fftLevelThreshold);
+      lastLevelThreshChange = mils;
+    } else if (fftLevelThreshold > minFFTLevelThreshold && (underDrawing || fftLevelThreshold > avgFrameValue) && mils - lastLevelThreshChange > adjustmentInterval) {
+      fftLevelThreshold--;
+      if (underDrawing) {
+        fftLevelThreshold = max(minFFTLevelThreshold, fftLevelThreshold + min(0, (maxFrameValue - fftLevelThreshold) / 10));
+      }
+      // logf("SoundPattern litCount = %i, frame value avg=%i,max=%i, fftLevelThreshold down to %i", litCount, avgFrameValue, maxFrameValue, fftLevelThreshold);
+      lastLevelThreshChange = mils;
+    }
   }
 };
 
